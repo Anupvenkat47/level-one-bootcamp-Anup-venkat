21932bfab5684b801fa51257b87a5aed8a2646e1
Anupvenkat47 <54064016+Anupvenkat47@users.noreply.github.com>
2021-02-03 15:28:41 +0530

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -4,3 +4,11 @@ void main()
 {
   printf("Hello World! My Name is Anup Venkat");
 }
+
+/*
+output for the given program is:
+ Hello World! My Name is Anup Venkat                                                                                                           
+                                                                                                                                              
+...Program finished with exit code 35                                                                                                         
+Press ENTER to exit console.
+*/
