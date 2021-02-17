f196519751dbacdfb675544167e2158fd48d25aa
Anupvenkat47 <54064016+Anupvenkat47@users.noreply.github.com>
2021-02-18 00:05:17 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -1,1 +1,14 @@
 //WAP to find the sum of n fractions.
+#include<stdio.h>
+int main()
+{
+  int n,i,sum=0;
+  printf("Enter the number of numbers you want to add: ");
+  scanf("%d", &n);
+  for(i=1;i<=num;i++)
+  {
+    sum=sum+i;
+  }
+  printf("The sum of first %d number = %d", n, sum);
+  return 0;
+}
