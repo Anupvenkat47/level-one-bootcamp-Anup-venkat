258eb2c0dd525a44a1a57f4db5db7b6d1c81e399
Anupvenkat47 <54064016+Anupvenkat47@users.noreply.github.com>
2021-02-17 21:42:16 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -1,1 +1,37 @@
 //WAP to find the distance between two points using structures and 4 functions.
+#include<stdio.h>
+#include<math.h>
+struct points
+{
+    int x,y;
+};
+int main()
+{
+  struct points a,b;
+  int distance;
+  printf("The Coordinates are inputted in the following order: x1, y1, x2, y2 \n");
+  a.x=input();
+  a.y=input();
+  b.x=input();
+  b.y=input();
+  distance=distcal(a.x,a.y,b.x,b.y);
+  output(a.x,a.y,b.x,b.y,distance);
+  return 0;
+}
+int input()
+{
+  int a;
+  printf("Enter the coordinate: ");
+  scanf("%d",&a);
+  return (a);
+}
+int distcal(int a.x, int a.y, int b.x, int b.y)
+{
+  int dist;
+  dist=sqrt((pow(b.x-a.x,2))+(pow(b.y-a.y,2)));
+  return (dist);
+}
+void output(int a.x,int a.y, int b.x, int b.y, int distance)
+{
+  printf("The distance between %d,%d and %d,%d is %d units", a.x, a.y, b.x, b.y, distance);
+}
