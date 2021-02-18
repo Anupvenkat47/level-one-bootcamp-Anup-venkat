9b91194c5cccc860a08ca3787452e41ccf8c2a97
Anupvenkat47 <54064016+Anupvenkat47@users.noreply.github.com>
2021-02-18 12:38:26 +0530

    Update n_fractions.c

--- n_fractions.c
+++ n_fractions.c
@@ -2,15 +2,16 @@
 #include<stdio.h>
 struct fraction
 {
-  int n,d;
+  int n,d,final;
 };
 int main()
 {
   struct fraction a,b,c,sum;
-  int n,i;
+  int n,i,gcdmain;
   printf("The numbers are entered in this order, First fraction numerator and denominator, then second numerator and denominator after that extra numerator and denominator are added if the user has specified it.\n");
   printf("Enter the number of fractions to be added (minimum 2): ");
   scanf("%d",&n);
+  n=n-2;
   a.n=input();
   a.d=input();
   b.n=input();
@@ -25,7 +26,8 @@ int main()
     sum.n=extraaddnum(c,sum);
     sum.d=extraaddden(c,sum);
   }
-  output(sum);
+  gcdmain=simplify(sum);
+  output(sum,gcdmain);
   return 0;
 }
 int input()
@@ -50,7 +52,7 @@ int firstaddden(struct fraction a, struct fraction b)
 int extraaddnum(struct fraction c, struct fraction sum)
 {
   int extranum;
-  extranum=((c.n+sum.d)+(sum.n+c.d));
+  extranum=((c.n*sum.d)+(sum.n*c.d));
   return (extranum);
 }
 int extraaddden(struct fraction c, struct fraction sum)
@@ -59,7 +61,18 @@ int extraaddden(struct fraction c, struct fraction sum)
   extraden=(c.d*sum.d);
   return (extraden);
 }
-void output(struct fraction sum)
+int simplify(struct fraction sum)
 {
-  printf("The sum is %d/%d", sum.n, sum.d);
+    int gcd,i;
+    for(i=1; i <= sum.n && i <= sum.d; ++i)
+    {
+    if(sum.n%i==0 && sum.d%i==0)
+        gcd = i;
+    }
+    return gcd;
 }
+void output(struct fraction sum,int gcdmain)
+{
+  printf("The sum is %d/%d", sum.n/gcdmain, sum.d/gcdmain);
+}
+
